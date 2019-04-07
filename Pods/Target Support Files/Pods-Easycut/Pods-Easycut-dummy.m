70c0e8c6c153dfb85b80b973a06615e25aa6bf8c
Thomas Martins <thomas.martins@epitech.eu>
2019-04-06 19:13:39 -0700

    clean project

--- Pods/Target Support Files/Pods-Easycut/Pods-Easycut-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Easycut : NSObject
-@end
-@implementation PodsDummy_Pods_Easycut
-@end
--- Pods/Target Support Files/Pods-EasycutUITests/Pods-EasycutUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_EasycutUITests : NSObject
-@end
-@implementation PodsDummy_Pods_EasycutUITests
-@end
